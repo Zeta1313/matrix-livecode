63ac293d43f6d332a4e674c27a8327d36c64b97a
Omnissiah <234093135+Zeta1313@users.noreply.github.com>
2026-04-16 23:06:25 -0700

    4/16 class

--- src/SalamanderSearch.java
+++ src/SalamanderSearch.java
@@ -1,4 +1,5 @@
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
 
 public class SalamanderSearch {
@@ -44,6 +45,58 @@ public class SalamanderSearch {
      * @throws IllegalArgumentException if the enclosure does not contain a salamander
      */
     public static boolean canReach(char[][] enclosure) {
+        int[] start = salamanderLocation(enclosure);
+        boolean[][] visisted = new boolean[enclosure.length][enclosure[0].length];
+        return canReach(enclosure, start, visisted);
+    }
+
+    private static boolean canReach(char[][] enclosure, int[] current, boolean[][] visited) {
+        int currentR = current[0];
+        int currentC = current[1];
+        if (enclosure[currentR][currentC] == 'f') return true;
+        if (visited[currentR][currentC]) return false;
+        visited[currentR][currentC] = true;
+        List<int[]> moves = possibleMoves(enclosure, current);
+
+        for (int[] x : moves) {
+            if (canReach(enclosure, x, visited)) return true;
+        }
         return false;
+    }
+
+    public static int[] salamanderLocation(char[][] enclosure) {
+        for (int i = 0; i < enclosure.length; i++) {
+            for (int j = 0; j < enclosure[0].length; j++) {
+                if (enclosure[i][j] == 's') {
+                    return new int[]{i, j};
+                }
+            }
+        }
+        throw new IllegalArgumentException("No salamander present");
+    }
+
+    public static List<int[]> possibleMoves(char[][] enclosure, int[] current) {
+        int currentR = current[0];
+        int currentC = current[1];
+
+        List<int[]> output = new ArrayList<int[]>();
+
+        int newR = currentR-1;
+        int newC = currentC;
+        if (newR >= 0 && enclosure[newR][newC] != 'W') output.add(new int[]{newR, newC});
+
+        newR = currentR+1;
+        newC = currentC;
+        if (newR < enclosure.length && enclosure[newR][newC] != 'W') output.add(new int[]{newR, newC});
+
+        newR = currentR;
+        newC = currentC-1;
+        if (newC >= 0 && enclosure[newR][newC] != 'W') output.add(new int[]{newR, newC});
+
+        newR = currentR;
+        newC = currentC+1;
+        if (newC < enclosure[0].length && enclosure[newR][newC] != 'W') output.add(new int[]{newR, newC});
+        
+        return output;
     }
 }
